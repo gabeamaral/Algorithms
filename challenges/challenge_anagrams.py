ab53667f0284d4ee127143751617c6ba34065c44
Gabriel Duarte <gabrielduarte1405@gmail.com>
2023-06-01 16:53:55 -0300

    Requisitos 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,21 @@
+def sort(string):
+    if len(string) < 2:
+        return string
+    pivot = string[0]
+    less = [i for i in string if i < pivot]
+    greater = [i for i in string if i > pivot]
+    equal = [i for i in string if i == pivot]
+    return sort("".join(less)) + "".join(equal) + sort("".join(greater))
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    sorted_first = sort(first_string.lower())
+    sorted_second = sort(second_string.lower())
+    if (
+        len(sorted_first) != len(sorted_second)
+        or not first_string
+        or not second_string
+    ):
+        return (sorted_first, sorted_second, False)
+    else:
+        return sorted_first, sorted_second, sorted_first == sorted_second
