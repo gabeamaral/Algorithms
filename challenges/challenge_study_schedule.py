457d8be608d300947875e68bf77664ec75650c62
Gabriel Duarte <gabrielduarte1405@gmail.com>
2023-06-01 17:06:58 -0300

    Corrigindo requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
-    if permanence_period is None:
-        return None
     count = 0
     for start_time, end_time in permanence_period:
+        if start_time is None or end_time is None:
+            return None
         if start_time <= target_time <= end_time:
             count += 1
     return count
