3dc02c65a69c0d981a46f2cb65988e1e887af25a
Gabriel Duarte <gabrielduarte1405@gmail.com>
2023-06-01 15:01:35 -0300

    Requisitos 1 e 3

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    if permanence_period is None:
+        return None
+    count = 0
+    for start_time, end_time in permanence_period:
+        if start_time <= target_time <= end_time:
+            count += 1
+    return count
